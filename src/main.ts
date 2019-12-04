4eb936e6140439b2a88b1dd7c1a9a3614b584570
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-12-04 10:42:24 -0800

    First pass at the Action

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,62 @@
-import * as core from '@actions/core';
-import {wait} from './wait'
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+function getNext(members, last) {
+  const lastIndex = members.findIndex(last)
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+  if (lastIndex == -1 || lastIndex + 1 >= members.length) {
+    return members[0]
+  } else {
+    return members[lastIndex + 1]
   }
 }
 
-run();
+async function getTeamMembers(token, teamName) {
+  const octokit = new github.GitHub(token)
+  const {data: teamData} = await octokit.teams.getByName(splitTeamName(teamName))
+  const {data: data} = await octokit.teams.listMembers({team_id: teamData.id})
+
+  return data.map(member => member.login)
+}
+
+function splitMembersList(list) {
+  return list.split(/\s+/).map(member => member[0] == '@' ? member.substring(1) : member)
+}
+
+function splitTeamName(teamName) {
+  const matches = teamName.match(/@?([^/]+)\/(.+)/)
+
+  return {org: matches[0], team_slug: matches[1]}
+}
+
+async function run() {
+  try {
+    const exclude = core.getInput('exclude')
+    const include = core.getInput('include')
+    const last = core.getInput('last')
+    const teamName = core.getInput('teamName')
+    const token = core.getInput('token')
+
+    let members: string[] = []
+
+    if (teamName) {
+      if (!token) {
+        throw new Error('`token` is required when `teamName` is supplied')
+      }
+
+      members = members.concat(await getTeamMembers(token, teamName))
+    }
+
+    members =
+      members.
+        concat(splitMembersList(include)).
+        filter(member => member in splitMembersList(exclude)).
+        sort()
+
+    core.setOutput('next', getNext(members, last))
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
