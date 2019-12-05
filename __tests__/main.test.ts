14c72f88ae313e79526735d4b1f35bd4e5cf1c49
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-12-05 09:59:15 -0800

    Reformat tests in describe/it style

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -4,56 +4,62 @@ import * as path from 'path'
 
 import { getNext, splitMembersList, splitTeamName } from '../src/main'
 
-test('getNext returns next element when last is in the member list', () => {
-  const next = getNext(['foo', 'bar', 'baz'], 'bar')
+describe('getNext', () => {
+  it('returns next element when last is in the member list', () => {
+    const next = getNext(['foo', 'bar', 'baz'], 'bar')
 
-  expect(next).toBe('baz')
+    expect(next).toBe('baz')
+  })
+
+  it('returns the first item in the list when last is the last member', () => {
+    const next = getNext(['foo', 'bar', 'baz'], 'baz')
+
+    expect(next).toBe('foo')
+  })
+
+  it('returns the first item in the list when last is null', () => {
+    const next = getNext(['foo', 'bar', 'baz'], null)
+
+    expect(next).toBe('foo')
+  })
+
+  it('returns the first item in the list when last is undefined', () => {
+    const next = getNext(['foo', 'bar', 'baz'], undefined)
+
+    expect(next).toBe('foo')
+  })
+
+  it('returns the first item in the list when last is not found', () => {
+    const next = getNext(['foo', 'bar', 'baz'], 'bamboozle')
+
+    expect(next).toBe('foo')
+  })
 })
 
-test('getNext returns the first item in the list when last is the last member', () => {
-  const next = getNext(['foo', 'bar', 'baz'], 'baz')
+describe('splitMembersList', () => {
+  it('splits on whitespace', () => {
+    const members = splitMembersList('foo bar baz')
 
-  expect(next).toBe('foo')
+    expect(members).toStrictEqual(['foo', 'bar', 'baz'])
+  })
+
+  it('strips at-signs from names', () => {
+    const members = splitMembersList('@foo @bar @baz')
+
+    expect(members).toStrictEqual(['foo', 'bar', 'baz'])
+  })
 })
 
-test('getNext returns the first item in the list when last is null', () => {
-  const next = getNext(['foo', 'bar', 'baz'], null)
+describe('splitTeamName', () => {
+  it('returns the appropriate object', () => {
+    const obj = splitTeamName('@foo/bar')
 
-  expect(next).toBe('foo')
-})
+    expect(obj).toStrictEqual({ org: 'foo', team_slug: 'bar' })
+  })
 
-test('getNext returns the first item in the list when last is undefined', () => {
-  const next = getNext(['foo', 'bar', 'baz'], undefined)
+  it('returns the appropriate object when no at-sign is used', () => {
+    const obj = splitTeamName('foo/bar')
 
-  expect(next).toBe('foo')
-})
-
-test('getNext returns the first item in the list when last is not found', () => {
-  const next = getNext(['foo', 'bar', 'baz'], 'bamboozle')
-
-  expect(next).toBe('foo')
-})
-
-test('splitMembersList splits on whitespace', () => {
-  const members = splitMembersList('foo bar baz')
-
-  expect(members).toStrictEqual(['foo', 'bar', 'baz'])
-})
-
-test('splitMembersList strips at-signs from names', () => {
-  const members = splitMembersList('@foo @bar @baz')
-
-  expect(members).toStrictEqual(['foo', 'bar', 'baz'])
-})
-
-test('splitTeamName returns the appropriate object', () => {
-  const obj = splitTeamName('@foo/bar')
-
-  expect(obj).toStrictEqual({ org: 'foo', team_slug: 'bar' })
-})
-
-test('splitTeamName returns the appropriate object when no at-sign is used', () => {
-  const obj = splitTeamName('foo/bar')
-
-  expect(obj).toStrictEqual({ org: 'foo', team_slug: 'bar' })
+    expect(obj).toStrictEqual({ org: 'foo', team_slug: 'bar' })
+  })
 })
