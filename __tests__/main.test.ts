08cf5243a824f8ceb479d235e0a34c285ac72cfe
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-12-05 10:00:55 -0800

    Remove unused imports

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,3 @@
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-
 import { getNext, splitMembersList, splitTeamName } from '../src/main'
 
 describe('getNext', () => {
