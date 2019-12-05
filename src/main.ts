afae12e33755528bc82f72866283ae52c3a7d6ec
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-12-05 10:14:51 -0800

    Add debugging statements

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 export function getNext(members, last) {
+  core.debug(`Members: ${JSON.stringify(members)}`)
+  core.debug(`Last: ${JSON.stringify(last)}`)
+
   const lastIndex = members.findIndex(member => member == last)
 
   if (lastIndex == -1 || lastIndex + 1 >= members.length) {
@@ -45,11 +48,14 @@ async function run() {
       }
 
       members = members.concat(await getTeamMembers(token, teamName))
+      core.debug(`Team members: ${JSON.stringify(members)}`)
     }
 
     members = members
       .concat(splitMembersList(include))
       .filter(member => member in splitMembersList(exclude))
+
+    core.debug(`Members (after include and exclude): ${JSON.stringify(members)}`)
 
     members = members.filter((item, index) => members.indexOf(item) === index).sort()
 
